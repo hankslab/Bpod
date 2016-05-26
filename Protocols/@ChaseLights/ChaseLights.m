9f443d4135f7ba4f299fb330b89592f6020aaa61
Jeffrey Erlich <jerlich@erlichlab.org>
2016-05-26 23:50:15 +0800

    - Create new class @ChaseLights - Add new properties to abstract class ProtoObj

--- Protocols/@ChaseLights/ChaseLights.m
+++ Protocols/@ChaseLights/ChaseLights.m
@@ -1,47 +1,23 @@
-classdef (Abstract) ProtoObj < handle
+classdef ChaseLights < ProtoObj
 % This class defines a Protocol Object. 
 % The methods of this class match the 'actions' that were implemented in BControl style classes.
 % Instantiations of this class should not directly access the global BpodSystem but rather get passed relevant elements of that global through methods.
 	properties (SetAccess=private,GetAccess=public)
-		settings % a struct with the protocol level settings
-		n_done_trials = 0;
-		n_started_trials = 0;
-		hit_history = [];
-		reward_history = [];
-		choice_history = [];
-		subjID
-		subjectname
-		sessiondate
-		sessionstart
-		sessionend
-		sessID
-		rigID
-
+		
 
 	end % properties
 
 	methods
-		function ProtocolObj(by, value)
+		function obj=LightChasing(by, value)
 			% by can be 'subject', 'subjID', 'settings' 
 			% if by == 'subject' value should be a string
 			% if by == 'subjID' value should be an int
 			% if by == 'settings' value should be a struct
 
+
+
 		end
 
-		init(obj)
-		
-		loadSettings(obj, by, value)
-		
-		prepareNextTrial(obj)
-		
-		SMA = generateSM(obj)
-		
-		trialCompleted(obj, RawEvents)
-		
-		saveTrial(obj, where)
-		
-		endSession(obj)		
 		
 	end % methods
 end % methods
